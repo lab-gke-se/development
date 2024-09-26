70eaf804c88d5a509ae102bb40b44b103c061d64
Dave Butland <dgbutland@gmail.com>
2024-09-26 15:16:19 +0100

    Added kms key

--- kms.tf
+++ kms.tf
@@ -1,17 +1,16 @@
-# module "prj_dev_network_kms_key_ring" {
-#   source = "github.com/capileus/kms//key_ring?ref=0.1"
+module "hpc_1_kms_key_ring" {
+  source = "github.com/lab-gke-se/modules//kms/key_ring?ref=main"
 
-#   name     = "prj-dev-network-key-ring"
-#   project  = local.projects["prj_dev_network"].project_id
-#   location = local.location
-# }
+  name     = "hpc-1-key-ring"
+  project  = local.projects["prj_dev_tenant_1"].project_id
+  location = "us-east4"
+}
 
-# module "prj_dev_network_kms_key" {
-#   source = "github.com/capileus/kms//key?ref=0.1"
+module "hpc_1_kms_key" {
+  source = "github.com/lab-gke-se/modules//kms/key?ref=main"
 
-#   name     = "prj-dev-network-key"
-#   project  = local.projects["prj_dev_network"].project_id
-#   key_ring = module.prj_dev_network_kms_key_ring.id
-#   services = local.prj_dev_network_services
-# }
-
+  name     = "hpc-1-key"
+  project  = local.projects["prj_dev_tenant_1"].project_id
+  key_ring = module.hpc_1_kms_key_ring.id
+  services = local.hpc_1_kms_services
+}
