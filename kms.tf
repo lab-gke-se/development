287f1f530f1c2da3112890d6bde5c6129780508d
Dave Butland <dgbutland@gmail.com>
2024-09-26 15:26:51 +0100

    Removed kms key

--- kms.tf
+++ kms.tf
@@ -1,16 +1,17 @@
-module "hpc_1_kms_key_ring" {
-  source = "github.com/lab-gke-se/modules//kms/key_ring?ref=main"
+# In bootstrap
+# module "hpc_1_kms_key_ring" {
+#   source = "github.com/lab-gke-se/modules//kms/key_ring?ref=main"
 
-  name     = "hpc-1-key-ring"
-  project  = local.projects["prj_dev_tenant_1"].project_id
-  location = "us-east4"
-}
+#   name     = "hpc-1-key-ring"
+#   project  = local.projects["prj_dev_tenant_1"].project_id
+#   location = "us-east4"
+# }
 
-module "hpc_1_kms_key" {
-  source = "github.com/lab-gke-se/modules//kms/key?ref=main"
+# module "hpc_1_kms_key" {
+#   source = "github.com/lab-gke-se/modules//kms/key?ref=main"
 
-  name     = "hpc-1-key"
-  project  = local.projects["prj_dev_tenant_1"].project_id
-  key_ring = module.hpc_1_kms_key_ring.id
-  services = local.hpc_1_kms_services
-}
+#   name     = "hpc-1-key"
+#   project  = local.projects["prj_dev_tenant_1"].project_id
+#   key_ring = module.hpc_1_kms_key_ring.id
+#   services = local.hpc_1_kms_services
+# }
